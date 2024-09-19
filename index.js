ba1af8874d90215af7ea5f6ab3e8a01f625d89d5
NixDevs <aniklakhany00@gmail.com>
2024-09-19 18:05:20 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const { Client, Collection } = require("discord.js");
+const { QuickDB } = require("quick.db");
+
+const client = new Client({ intents: 32767 });
+module.exports = client;
+
+
+client.db = new QuickDB()
+
+// Global Variables
+client.commands = new Collection();
+client.slashCommands = new Collection();
+
+// Initializing the project
+require("./handler")(client);
+client.login(process.env.TOKEN);
